725a030a9b42f6687cf8e3aa68a27f34366523ea
ajaykumar reddy <ajaykumarreddy366@gmail.com>
2023-02-20 06:20:08 +0000

    Update file junit.java

--- junit.java
+++ junit.java
@@ -1,5 +1,5 @@
 public class junit{
-    public static void main(string[] args){
-        system.out.println("hello");
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
