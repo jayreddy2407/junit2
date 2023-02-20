507f5cb149a831f65e5f1d3e74faff804e4cd89f
jayreddy2407 <124673436+jayreddy2407@users.noreply.github.com>
2023-02-20 14:17:06 +0530

    Delete junit.java

--- junit.java
+++
@@ -1,8 +0,0 @@
-public class junit{
-    public static void main(String[] args){
-        int junit=30;
-        string str="ajay";
-        System.out.println("hello");
-        system.out.println(junit+"  "+str);
-    }
-}
