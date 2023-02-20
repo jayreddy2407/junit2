e4a7ffedf69dd6985b07b2dcb3a99018939bc0c0
jayreddy2407 <124673436+jayreddy2407@users.noreply.github.com>
2023-02-20 14:12:11 +0530

    Update junit.java

--- junit.java
+++ junit.java
@@ -1,6 +1,6 @@
 public class junit{
     public static void main(String[] args){
-        int junit=20;
+        int junit=30;
         System.out.println("hello");
         system.out.println(junit);
     }
